23672c7182a5587a8c2938187d7936f32c404711
maheshshali25 <maiya9986@gmail.com>
2024-05-20 12:51:26 +0530

    first

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cors  = require('cors')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+dotenv.config()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use("/", (req, res) => {
+    res.json({
+        status: "API working Fine",
+        code: 200
+    })
+})
+
+app.listen(() => app.listen(PORT, () => console.log(`Server started on port: ${PORT}`)))
